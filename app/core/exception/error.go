5257651235cd463d288661eb5baac5aa167b79e1
wsj <essential2189@gmail.com>
2024-01-23 14:30:12 +0900

    feat: error response generator

--- app/core/exception/error.go
+++ app/core/exception/error.go
@@ -1,21 +1,41 @@
 package exception
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Error struct {
-	Code errorCode   `json:"code"`
-	Err  error       `json:"error"`
-	Data interface{} `json:"data"`
+	Code errorCode
+	Err  error
+	Data interface{}
+}
+
+type ErrorResponse struct {
+	Code    string      `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
 func (e *Error) Error() string {
-	return fmt.Sprintf("code: %s, error: %s, data: %+v", e.Code, e.Err.Error(), e.Data)
+	return fmt.Sprintf("code: %s, message: %s, error: %s, data: %+v", e.Code, ErrorMessage()(e.Code), e.Err.Error(), e.Data)
 }
 
 func Wrap(code errorCode, err error, data interface{}) *Error {
+	if err == nil {
+		return nil
+	}
+
 	return &Error{
 		Code: code,
 		Err:  err,
 		Data: data,
+	}
+}
+
+func GenerateErrorResponse(e Error) *ErrorResponse {
+	return &ErrorResponse{
+		Code:    string(e.Code),
+		Message: ErrorMessage()(e.Code),
+		Data:    e.Data,
 	}
 }
--- app/core/exception/error_handler.go
+++ app/core/exception/error_handler.go
@@ -9,18 +9,30 @@ import (
 )
 
 func ErrorHandler(c *fiber.Ctx, err error) error {
-	code := fiber.StatusInternalServerError
-	message := "Internal Server Error"
-	var data interface{}
+	c.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
 
-	var customError *Error
-	ok := errors.As(err, &customError)
-	if ok {
-		code, _ = strconv.Atoi(strings.Split(string(customError.Code), ".")[0])
-		message = ErrorMessage()(customError.Code)
-		data = customError.Data
+	const defaultErrorCode = fiber.StatusInternalServerError
+	const defaultErrorMessage = "Internal Server Error"
+
+	extractStatusCode := func(customError *Error) int {
+		parts := strings.Split(string(customError.Code), ".")
+		if len(parts) > 0 {
+			if code, err := strconv.Atoi(parts[0]); err == nil {
+				return code
+			}
+		}
+		return defaultErrorCode
 	}
 
-	c.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
-	return c.Status(code).JSON(fiber.Map{"error": code, "message": message, "data": data})
+	var customError *Error
+	if errors.As(err, &customError) {
+		errorResponse := GenerateErrorResponse(*customError)
+		return c.Status(extractStatusCode(customError)).JSON(errorResponse)
+	}
+
+	return c.Status(defaultErrorCode).JSON(fiber.Map{
+		"code":  defaultErrorCode,
+		"error": defaultErrorMessage,
+		"data":  nil,
+	})
 }
